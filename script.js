3df41ac848c7856c4ccd12afd527a73fdd5e9306
Chaithu036 <uchaitanyavihar@gmail.com>
2025-04-02 10:42:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.navLinks');
+
+menuIcon.onclick = ()=> {
+    navLinks.classList.toogle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
